29a8e3e2839b258189356d8b4e80cbb2de80aca2
Avinash Kumar A <90566941+avinashkallingal@users.noreply.github.com>
2024-07-22 15:47:15 +0530

    added initial

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express()
 
 app.get('/',(req,res)=>{
 
-    res.send("hii i am avinash")
+    res.send("hii i am avinash a")
     console.log("this is server")
     console.log("chnge")
 })
